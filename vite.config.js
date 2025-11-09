122ef8e7ccc540fa5f9b04e9c22f238e11ca2e06
Vidya <vidyaraut17297@gmail.com>
2025-11-10 03:12:06 +0530

    "fix-github-pages-404-favicon-meta-tags"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' && process.env.GITHUB_PAGES === 'true' ? '/vidyaraut/' : '/',
+  base: mode === 'production' ? '/vidyaraut/' : '/',
   resolve: {
     alias: {
       '@': '/src',
